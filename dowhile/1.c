564344df19176cff6b436fd00681e1d5fad6b602
vijay1923 <vijaymagadum1923@gmail.com>
2026-05-22 09:57:32 +0530

    Initial Commit ~

---
+++ dowhile/1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	/*	int a=2;
+		do
+		{
+		printf("%d ",a);
+		a++;
+		if(a>5);
+		break;
+		}while(1);
+		}
+
+	 */
+
+int i=1;
+do
+{
+	printf("%d\n",i);
+i++;
+}while(i<=5);
+}
---
+++ dowhile/continue.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main ()
+{
+	int i;
+	for(i=0;i<=5;i++)
+	{
+		if (i==3)
+		{	continue;
+		}
+		printf("%d\n",i);
+	}
+
+
+}
